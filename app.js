d33f3c2f1906ee3f9e638cb219261825100caf53
umutlion <umutcayli@outlook.com>
2021-12-24 03:50:50 +0300

    mongodb installation and template engine views added.

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const path = require('path');
-
+const ejs = require('ejs');
 const app = express();
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 app.use(express.static('public'));
 
+// ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public/index.html'))
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 const port = 3000;
