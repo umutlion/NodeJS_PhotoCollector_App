8fd17461f357b53a10ca7937cd851ab0486df0be
umutlion <umutcayli@outlook.com>
2021-12-25 22:45:47 +0300

    update photo.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const Photo = require('./models/Photo');
 const fs = require('fs');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 
 // CONNECT DB
 
@@ -22,6 +23,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 // ROUTES
 app.get('/', async (req, res) => {
@@ -63,6 +65,20 @@ app.get('/photos/:id', async (req, res) => {
   res.render('photo', {
     photo,
   });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', { photo });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
 });
 
 const port = 3000;
